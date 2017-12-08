64f1bd7bc0a69505ebd20f1479371ea82c6280cc
alexey.dovgal <alexey.dovgal@effective-soft.com>
2017-12-08 17:05:31 +0300

    08122017

--- src/js/main.js
+++ src/js/main.js
@@ -1,15 +1,49 @@
-var slider = new Swiper('.swiper-container', {
-    slidesPerView: 3,
-    breakpoints: {
-        1056:{
-            slidesPerView: 2
+(function () {
+    var slider = new Swiper('.swiper-container', {
+        slidesPerView: 3,
+        spaceBetween: 10,
+        breakpoints: {
+            676: {
+                slidesPerView: 2
+            }
         },
-        676:{
-            slidesPerView: 1
+        navigation: {
+            nextEl: '.slider-button-next',
+            prevEl: '.slider-button-prev'
         }
-    },
-    navigation: {
-        nextEl: '.slider-button-next',
-        prevEl: '.slider-button-prev'
-    }
-});
+    });
+
+
+   /* var galleryTop = new Swiper('.gallery-top', {
+        spaceBetween: 10,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        }
+    });
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+        centeredSlides: false,
+        slidesPerView: 4,
+        noSwiping: true,
+        touchRatio: 0.2,
+        slideToClickedSlide: true
+    });
+    galleryTop[0].controller.control = galleryThumbs;
+    galleryThumbs[1].controller.control = galleryTop;*/
+    var galleryTop = new Swiper('.gallery-top', {
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+    var galleryThumbs = new Swiper('.gallery-thumbs', {
+        slidesPerView: 3,
+        slidesPerGroup: 3,
+        loop: true,
+        loopFillGroupWithBlank: true,
+        touchRatio: 0.2,
+        slideToClickedSlide: true,
+    });
+    galleryTop.controller.control = galleryThumbs;
+    galleryThumbs.controller.control = galleryTop;
+}());
