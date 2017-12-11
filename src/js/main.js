7465fc5dc3c4b548bc39b41333368691c770ae87
alexey.dovgal <alexey.dovgal@effective-soft.com>
2017-12-11 13:03:57 +0300

    refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -1,52 +1,16 @@
-(function () {
-    var slider = new Swiper('.swiper-container', {
-        slidesPerView: 3,
-        spaceBetween: 10,
-        breakpoints: {
-            676: {
-                slidesPerView: 2
-            }
-        },
-        navigation: {
-            nextEl: '.slider-button-next',
-            prevEl: '.slider-button-prev'
+var slider = new Swiper('.swiper-container', {
+    slidesPerView: 3,
+    spaceBetween: 10,
+    breakpoints: {
+        676: {
+            slidesPerView: 2
         }
-    });
-
-
-   /* var galleryTop = new Swiper('.gallery-top', {
-        spaceBetween: 10,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev'
-        }
-    });
-    var galleryThumbs = new Swiper('.gallery-thumbs', {
-        centeredSlides: false,
-        slidesPerView: 4,
-        noSwiping: true,
-        touchRatio: 0.2,
-        slideToClickedSlide: true
-    });
-    galleryTop[0].controller.control = galleryThumbs;
-    galleryThumbs[1].controller.control = galleryTop;*/
-    /*var galleryTop = new Swiper('.gallery-top', {
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-    });
-    var galleryThumbs = new Swiper('.gallery-thumbs', {
-        slidesPerView: 3,
-        slidesPerGroup: 3,
-        loop: true,
-        loopFillGroupWithBlank: true,
-        touchRatio: 0.2,
-        slideToClickedSlide: true,
-    });
-    galleryTop.controller.control = galleryThumbs;
-    galleryThumbs.controller.control = galleryTop;*/
-}());
+    },
+    navigation: {
+        nextEl: '.slider-button-next',
+        prevEl: '.slider-button-prev'
+    }
+});
 $('.slider-for').slick({
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -58,7 +22,7 @@ $('.slider-nav').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     arrows: false,
-    variableWidth: true,
+    variableWidth: false,
     asNavFor: '.slider-for',
     centerMode: false,
     focusOnSelect: true
